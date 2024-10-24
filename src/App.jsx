d40bb03f4bc3a2b64de0c09fb68df704dceed2c1
Ahammod <ahammodullah12@gmail.com>
2024-10-25 01:56:41 +0600

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-    <div className=" min-h-screen w-full bg-slate-800 bg-center bg-cover px-20 py-2 relative">
+    <div className=" h-full w-full bg-slate-800 bg-cover px-20 py-2 relative">
       <nav className="flex items-center">
          <p className="text-white p-5 text-2xl">Ahammod</p>
 
@@ -29,7 +29,7 @@ function App() {
         <button className="bg-blue-500 px-5 w-19 h-10  text-white  m-3 rounded-3xl hover:bg-red-400 duration-500">JOIN US<span className="inline-block">&#8594;</span></button>
       </nav>
 
-        <div className='flex justify-between'>
+        <div className='flex justify-between '>
         <div>
        <div className="mt-20 "> 
         <h6 className="text-blue-600 mb-5">online traning</h6>
@@ -44,7 +44,7 @@ function App() {
        </div>
 
        <div>
-       <img src={image1} alt="logo" className="xl:absolute bottom-0 right-20" />
+       <img src={image1} alt="logo" className="" />
        </div>
         </div>
        
